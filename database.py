61f26343a0a2a9292f5bf9f146b31eebdda1a137
sri <srilekhya@gmail.com>
2024-06-07 19:28:20 +0530

    Merge changes from GitHub

--- database.py
+++ database.py
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
-=======
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
->>>>>>> 89900e3c157d554cd02bbe13312b712e97820d9b
