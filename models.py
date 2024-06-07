bf017ec251c07b1d95b45f194b35947d82e804da
sri <srilekhya@gmail.com>
2024-06-07 18:12:33 +0530

    first commit

---
+++ models.py
@@ -0,0 +1,16 @@
+from database import db
+
+class ContentOffering(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(500), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+
+class Cart(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content_id = db.Column(db.Integer, db.ForeignKey('content_offering.id'), nullable=False)
+    content = db.relationship('ContentOffering', backref=db.backref('cart_items', lazy=True))
+
+class Transaction(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    total_price = db.Column(db.Float, nullable=False)
